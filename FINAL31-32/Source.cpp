b9322481ab5224d7baa07a6dd09f0b2eda69fb22
dedzeo <37904173+dedzeo@users.noreply.github.com>
2018-04-15 12:57:39 +0700

    Add files via upload

--- FINAL31-32/Source.cpp
+++ FINAL31-32/Source.cpp
@@ -7,25 +7,11 @@ struct student {
 	string name;
 	string checked[10];
 };
-void checkin(student*&st, int k) {
-	string id;
-	cout << "your student ID: ";
-	getline(cin, id, '\n');
-	
-	int w;
-	cout << "what week is it? Week:" << endl;
-	cin >> w;
-	for (int i = 0; i < k; i++) {
-		if (st[i].id == id) {
-			cout << "lets check in...";
-			st[i].checked[w - 1] = 'o';
-			cout << endl << "checked in!" << endl;
-			return;
-		}
-	}
-	cout << "no student with this ID, returning";
+void checkin(student*&st, int i) {
+	int w = 10;//week 10
+	st[i].checked[w - 1] = 'o';
 }
-void view(student*st,int k, string a) {
+void update(student*st,int k, string a) {
 	ofstream fout;
 	cout << "updating..." << endl;
 	fout.open(a);
@@ -45,18 +31,28 @@ void view(student*st,int k, string a) {
 			cout << st[i].checked[j] << ",";
 		}
 		if (i == k - 1) {
-			fout << st[i].checked[k];
-			cout << st[i].checked[k];
+			fout << st[i].checked[9];
+			cout << st[i].checked[9];
 		}
 		else {
-			fout << st[i].checked[k] << endl; 
-			cout << st[i].checked[k] << endl;
+			fout << st[i].checked[9] << endl; 
+			cout << st[i].checked[9] << endl;
 		}
 	}
 	cout << endl << "updated" << endl;
 	fout.close();
 }
-
+void viewresult(student*st, int i, string a) {
+	cout << "updating..." << endl;
+	cout << "ID,name,Week1,week2,week3,week3,week4,week5,week6,week7,week8,week9,week10" << endl;
+	cout << st[i].id << ",";
+	cout << st[i].name << ",";
+	int j = 0;
+	for (j = 0; j < 10; j++) {
+		cout << st[i].checked[j] << ",";
+	}
+	cout << endl << "viewed" << endl;
+}
 int main() {
 	string clas, course;
 	ifstream fin;
@@ -68,7 +64,7 @@ int main() {
 	fin.open(a);
 	if (!fin.is_open()) {
 		cout << "wrong please run again" << endl;
-		return;
+		return 0;
 	}
 	getline(fin, clas, '\n');
 	student *st;
@@ -88,8 +84,25 @@ int main() {
 		k++;
 	}
 	fin.close();
-	checkin(st, k);
-	view(st, k, a);
+	string id;
+	cout << "student ID: ";
+	getline(cin, id, '\n');
+	for (int i = 0; i < k; i++) {
+		if (st[i].id == id) {
+			cout << "lets check in...";
+			checkin(st, i);
+			update(st, k, a);
+			cout << endl << "checked in!" << endl;
+		}
+	}
+	cout << "checkin result of student's student ID: " << endl;
+	getline(cin, id, '\n');
+	for (int i = 0; i < k; i++) {
+		if (st[i].id == id) {
+			viewresult(st, i, a);
+		}
+	}
+	
 	cin >> clas;
 	delete[]st;
 	return 0;
